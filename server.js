27b3c80bfffccee259918278e00414d79f390cdb
saymarizvi <rizvisayma@yahoo.com>
2021-10-03 02:14:16 +0530

    clustering in nodejs

--- server.js
+++ server.js
@@ -3,34 +3,37 @@ const express = require('express');
 const app = express();
 
 const cluster = require('cluster');
+const os = require('os');
 //for windows
 cluster.schedulingPolicy = cluster.SCHED_RR;
 
 
-function delay(duration){
+function delay(duration) {
     const startTime = Date.now();
-    while(Date.now()- startTime < duration){
+    while (Date.now() - startTime < duration) {
         //event loop is blocked...
     }
 }
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send(`Performance example: ${process.pid}`);
 });
 
 
-app.get('/timer',(req,res) =>{
+app.get('/timer', (req, res) => {
     delay(9000);
     res.send(`Ding ding ding: ${process.pid}`)
 });
 
-if(cluster.isMaster){
+if (cluster.isMaster) {
     console.log('Master has been started');
-    cluster.fork();
-    cluster.fork();
-}else{
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+        cluster.fork();
+    }
+} else {
     console.log('Worker process started');
     app.listen(3000);
-  
+
 }
 
